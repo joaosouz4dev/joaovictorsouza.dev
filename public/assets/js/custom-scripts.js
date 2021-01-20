db6f83ae4cba6441145eeaa47b7874f059dabbd9
João Victor Souza <souza.joao1997@gmail.com>
2021-01-20 16:19:53 -0300

    update scroll

--- public/assets/js/custom-scripts.js
+++ public/assets/js/custom-scripts.js
@@ -1,1 +1,388 @@
-!function(e){"use strict";e.fn.andSelf=function(){return this.addBack.apply(this,arguments)},e(window).on("load",function(){e(".section-loader").fadeOut("slow");var a=e(".portfolioContainer");a.isotope({filter:"*",animationOptions:{queue:!0}}),e(".portfolio-nav li").click(function(){e(".portfolio-nav .current").removeClass("current"),e(this).addClass("current");var t=e(this).attr("data-filter");return a.isotope({filter:t,animationOptions:{queue:!0}}),!1})});e(".navbar-toggler");var a=e(".overlay"),t=e(".navbar-collapse");function s(){e("#contactForm").removeClass().addClass("shake animated").one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",function(){e(this).removeClass()})}function i(a,t){if(a)var s="h3 text-center fadeInUp animated text-success";else s="h3 text-center shake animated text-danger";e("#msgSubmit").removeClass().addClass(s).text(t)}e(".navbar-toggler, .navbar-nav li a, .overlay").on("click",function(){e(".navbar-toggler").toggleClass("active"),a.toggleClass("active"),t.toggleClass("active")}),e("#jv-header").onePageNav({currentClass:"active",changeHash:!1,scrollSpeed:750,scrollThreshold:.5}),e("[data-fancybox]").fancybox({}),new WOW({mobile:!1}).init(),e(window).on("scroll",function(){e(window).scrollTop()>=50?e(".nav-scroll").addClass("nav-strict"):e(".nav-scroll").removeClass("nav-strict")}),e(".determinate").each(function(){var a=e(this).text();e(this).css("width",a).empty().append('<i class="fa fa-circle"></i>')}),e("#portfolio-item").mixItUp(),e("#jv-client-review").owlCarousel({loop:!1,responsiveClass:!0,nav:!0,autoplay:!1,smartSpeed:450,stopOnHover:!0,animateIn:"slideInRight",animateOut:"slideOutLeft",autoplayHoverPause:!0,responsive:{0:{items:1},768:{items:2},1170:{items:3}}}),e(".jv-project-testimonial").owlCarousel({loop:!0,responsiveClass:!0,nav:!1,dots:!1,autoplay:!0,smartSpeed:450,stopOnHover:!0,animateIn:"slideInRight",animateOut:"slideOutLeft",autoplayHoverPause:!0,pagination:!1,responsive:{0:{items:1},768:{items:1},1170:{items:1}}}),e("#single-project").owlCarousel({loop:!1,responsiveClass:!0,nav:!1,dots:!0,autoplay:!1,smartSpeed:450,stopOnHover:!0,animateIn:"slideInRight",animateOut:"slideOutLeft",autoplayHoverPause:!0,pagination:!1,responsive:{0:{items:1},768:{items:1},1170:{items:1}}}),e(".jv-single-project-slide-by-side").owlCarousel({loop:!1,responsiveClass:!0,nav:!0,navText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],dots:!1,autoplay:!1,smartSpeed:450,stopOnHover:!0,animateIn:"slideInRight",animateOut:"slideOutLeft",autoplayHoverPause:!0,pagination:!1,responsive:{0:{items:1},768:{items:1},1170:{items:1}}}),e("#jv-single-client-review").owlCarousel({loop:!1,responsiveClass:!0,nav:!0,autoplay:!1,smartSpeed:450,stopOnHover:!0,animateIn:"slideInRight",animateOut:"slideOutLeft",autoplayHoverPause:!0,responsive:{0:{items:1},768:{items:1},1170:{items:1}}}),e("#jv-2-client-review").owlCarousel({loop:!1,responsiveClass:!0,nav:!0,autoplay:!1,smartSpeed:450,stopOnHover:!0,animateIn:"slideInRight",animateOut:"slideOutLeft",autoplayHoverPause:!0,responsive:{0:{items:1},768:{items:2},1170:{items:2}}}),e("#contactForm").validator().on("submit",function(a){var t,o,n;a.isDefaultPrevented()?(s(),i(!1,"Did you fill in the form properly?")):(a.preventDefault(),t=e("#name").val(),o=e("#email").val(),n=e("#message").val(),e.ajax({type:"POST",url:"process.php",data:"name="+t+"&email="+o+"&message="+n,success:function(a){"success"==a?(e("#contactForm")[0].reset(),i(!0,"Message Sent!")):(s(),i(!1,a))}}))})}(jQuery);
+(function($) {
+	'use strict';
+
+	$.fn.andSelf = function() {
+		return this.addBack.apply(this, arguments);
+	};
+
+	/* Loader Code Start */
+	$(window).on('load', function() {
+		$('.section-loader').fadeOut('slow');
+
+		var $container = $('.portfolioContainer');
+		$container.isotope({
+			filter: '*',
+			animationOptions: {
+				queue: true
+			}
+		});
+
+		$('.portfolio-nav li').click(function() {
+			$('.portfolio-nav .current').removeClass('current');
+			$(this).addClass('current');
+
+			var selector = $(this).attr('data-filter');
+			$container.isotope({
+				filter: selector,
+				animationOptions: {
+					queue: true
+				}
+			});
+			return false;
+		});
+	});
+	/* Loader Code End */
+
+	/*
+    |====================
+    | Mobile NAv trigger
+    |=====================
+    */
+
+	var trigger = $('.navbar-toggler'),
+		overlay = $('.overlay'),
+		navc = $('.navbar-collapse'),
+		active = false;
+
+	$('.navbar-toggler, .navbar-nav li a, .overlay').on('click', function() {
+		$('.navbar-toggler').toggleClass('active');
+		//   $('#js-navbar-menu').toggleClass('active');
+		//   $('.navbar-collapse').toggleClass('show');
+		overlay.toggleClass('active');
+		navc.toggleClass('active');
+	});
+
+	/*
+    |=================
+    | Onepage Nav
+    |================
+    */
+	$('#jv-header').onePageNav({
+		currentClass: 'active',
+		changeHash: false,
+		scrollSpeed: 750,
+		scrollThreshold: 0.5
+	});
+
+	/*
+    |=================
+    | fancybox
+    |================
+    */
+	$('[data-fancybox]').fancybox({});
+
+	/*
+    |===============
+    | WOW ANIMATION
+    |==================
+    */
+	var wow = new WOW({
+		mobile: false // trigger animations on mobile devices (default is true)
+	});
+	wow.init();
+
+	/*
+    |=================
+    | AOS
+    |================
+    */
+
+	//AOS.init();
+
+	/*
+    | ==========================
+    | NAV FIXED ON SCROLL
+    | ==========================
+    */
+	var validator = false;
+	$(window).on('scroll', function() {
+		if (!validator) {
+			validator = true;
+			$('#jv-header').onePageNav({
+				currentClass: 'active',
+				changeHash: false,
+				scrollSpeed: 750,
+				scrollThreshold: 0.5
+			});
+		}
+		var scroll = $(window).scrollTop();
+		if (scroll >= 50) {
+			$('.nav-scroll').addClass('nav-strict');
+		} else {
+			$('.nav-scroll').removeClass('nav-strict');
+		}
+	});
+
+	/*
+    |=================
+    | Progress bar
+    |================
+    */
+
+	$('.determinate').each(function() {
+		var width = $(this).text();
+		$(this).css('width', width).empty().append('<i class="fa fa-circle"></i>');
+	});
+
+	/*
+    |=================
+    | Portfolio mixin
+    |================
+    */
+
+	$('#portfolio-item').mixItUp();
+
+	/*
+    |=================
+    | Client review
+    |================
+    */
+
+	$('#jv-client-review').owlCarousel({
+		loop: false,
+		responsiveClass: true,
+		nav: true,
+		autoplay: false,
+		smartSpeed: 450,
+		stopOnHover: true,
+		animateIn: 'slideInRight',
+		animateOut: 'slideOutLeft',
+		autoplayHoverPause: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			768: {
+				items: 2
+			},
+			1170: {
+				items: 3
+			}
+		}
+	});
+
+	/*
+    |=================
+    | Project review slide
+    |================
+    */
+
+	$('.jv-project-testimonial').owlCarousel({
+		loop: true,
+		responsiveClass: true,
+		nav: false,
+		dots: false,
+		autoplay: true,
+		smartSpeed: 450,
+		stopOnHover: true,
+		animateIn: 'slideInRight',
+		animateOut: 'slideOutLeft',
+		autoplayHoverPause: true,
+		pagination: false,
+		responsive: {
+			0: {
+				items: 1
+			},
+			768: {
+				items: 1
+			},
+			1170: {
+				items: 1
+			}
+		}
+	});
+
+	/*
+    |=================
+    | Single Project review
+    |================
+    */
+
+	$('#single-project').owlCarousel({
+		loop: false,
+		responsiveClass: true,
+		nav: false,
+		dots: true,
+		autoplay: false,
+		smartSpeed: 450,
+		stopOnHover: true,
+		animateIn: 'slideInRight',
+		animateOut: 'slideOutLeft',
+		autoplayHoverPause: true,
+		pagination: false,
+		responsive: {
+			0: {
+				items: 1
+			},
+			768: {
+				items: 1
+			},
+			1170: {
+				items: 1
+			}
+		}
+	});
+
+	/*
+    |=================
+    | Project review slide
+    |================
+    */
+
+	$('.jv-single-project-slide-by-side').owlCarousel({
+		loop: false,
+		responsiveClass: true,
+		nav: true,
+		navText: [ "<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>" ],
+		dots: false,
+		autoplay: false,
+		smartSpeed: 450,
+		stopOnHover: true,
+		animateIn: 'slideInRight',
+		animateOut: 'slideOutLeft',
+		autoplayHoverPause: true,
+		pagination: false,
+		responsive: {
+			0: {
+				items: 1
+			},
+			768: {
+				items: 1
+			},
+			1170: {
+				items: 1
+			}
+		}
+	});
+
+	/*
+    |=================
+    | Single client review
+    |================
+    */
+
+	$('#jv-single-client-review').owlCarousel({
+		loop: false,
+		responsiveClass: true,
+		nav: true,
+		autoplay: false,
+		smartSpeed: 450,
+		stopOnHover: true,
+		animateIn: 'slideInRight',
+		animateOut: 'slideOutLeft',
+		autoplayHoverPause: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			768: {
+				items: 1
+			},
+			1170: {
+				items: 1
+			}
+		}
+	});
+
+	/*
+    |=================
+    | Clint review slide
+    |================
+    */
+
+	$('#jv-2-client-review').owlCarousel({
+		loop: false,
+		responsiveClass: true,
+		nav: true,
+		autoplay: false,
+		smartSpeed: 450,
+		stopOnHover: true,
+		animateIn: 'slideInRight',
+		animateOut: 'slideOutLeft',
+		autoplayHoverPause: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			768: {
+				items: 2
+			},
+			1170: {
+				items: 2
+			}
+		}
+	});
+
+	// Smooth Scroll
+	// $(function() {
+	//   $('a[href*=#]:not([href=#])').click(function() {
+	//     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	//       var target = $(this.hash);
+	//       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	//       if (target.length) {
+	//         $('html,body').animate({
+	//           scrollTop: target.offset().top
+	//         }, 600);
+	//         return false;
+	//       }
+	//     }
+	//   });
+	// });
+
+	/*
+    |=================
+    | CONTACT FORM
+    |=================
+    */
+
+	$('#contactForm').validator().on('submit', function(event) {
+		if (event.isDefaultPrevented()) {
+			// handle the invalid form...
+			formError();
+			submitMSG(false, 'Did you fill in the form properly?');
+		} else {
+			// everything looks good!
+			event.preventDefault();
+			submitForm();
+		}
+	});
+
+	function submitForm() {
+		var name = $('#name').val();
+		var email = $('#email').val();
+		var message = $('#message').val();
+		$.ajax({
+			type: 'POST',
+			url: 'process.php',
+			data: 'name=' + name + '&email=' + email + '&message=' + message,
+			success: function(text) {
+				if (text == 'success') {
+					formSuccess();
+				} else {
+					formError();
+					submitMSG(false, text);
+				}
+			}
+		});
+	}
+	function formSuccess() {
+		$('#contactForm')[0].reset();
+		submitMSG(true, 'Message Sent!');
+	}
+	function formError() {
+		$('#contactForm')
+			.removeClass()
+			.addClass('shake animated')
+			.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+				$(this).removeClass();
+			});
+	}
+	function submitMSG(valid, msg) {
+		if (valid) {
+			var msgClasses = 'h3 text-center fadeInUp animated text-success';
+		} else {
+			var msgClasses = 'h3 text-center shake animated text-danger';
+		}
+		$('#msgSubmit').removeClass().addClass(msgClasses).text(msg);
+	}
+})(jQuery);
