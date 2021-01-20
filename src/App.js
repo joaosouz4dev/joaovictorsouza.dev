a367714b7468ccd03ff4a6fecafeee74cb304179
João Victor Souza <souza.joao1997@gmail.com>
2021-01-20 17:04:25 -0300

    menu correto

--- src/App.js
+++ src/App.js
@@ -29,8 +29,8 @@ function App() {
 
 							<div class="collapse navbar-collapse" id="navbarSupportedContent">
 								<ul class="navbar-nav mr-auto ml-auto">
-									<li class="nav-item">
-										<a class="nav-item active" href="#jv-home">
+									<li class="nav-item active">
+										<a class="nav-link" href="#jv-home">
 											Inicio
 										</a>
 									</li>
