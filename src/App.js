d133ef1fe44441b6d42ac5a74dfb393da144f700
João Victor Souza <souza.joao1997@gmail.com>
2024-02-05 19:33:54 -0300

    feat: adicionando analitics

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import "./assets/css/style.css";
+import './assets/css/style.css';
 
-import Home from "./routes/index";
-import Matrix from "./routes/matrix/index";
-import NotFound from "./routes/404/index";
-import Wpp from "./routes/wpp/index";
-import Converta from "./routes/converta";
-import Privacidade from "./routes/privacidade";
+import Home from './routes/index';
+import Matrix from './routes/matrix/index';
+import NotFound from './routes/404/index';
+import Wpp from './routes/wpp/index';
+import Converta from './routes/converta';
+import Privacidade from './routes/privacidade';
+import { Analytics } from '@vercel/analytics/react';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/zap" element={<Wpp />} />
-        <Route exact path="/matrix" element={<Matrix />} />
-        <Route exact path="/converta/ibivagas" element={<Converta />} />
-        <Route
-          exact
-          path="/politica-de-privacidade/:title"
-          element={<Privacidade />}
-        />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <>
+      <Analytics />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/zap" element={<Wpp />} />
+          <Route exact path="/matrix" element={<Matrix />} />
+          <Route exact path="/converta/ibivagas" element={<Converta />} />
+          <Route exact path="/politica-de-privacidade/:title" element={<Privacidade />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </>
   );
 };
 
