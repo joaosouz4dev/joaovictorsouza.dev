910318fb0d92272642a2da5bb5273a19e1ef3747
João Victor Souza <souza.joao1997@gmail.com>
2024-02-22 18:35:03 -0300

    feat: change page ibivagas

--- src/pages/converta/index.js
+++ src/pages/converta/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Converta() {
   React.useEffect(() => {
-    window.location.href = "https://ibivagas.com.br/temporario";
+    window.location.href = "https://ibivagas.com.br/cad-app";
   }, []);
 
   return (
